ec9665e83617ba10f8cac61f39d624f330282ea1
Sanya <sanya2371@gmail.com>
2017-10-17 20:29:58 +0300

    Add list.

--- SelectListDemo/SelectListDemo/ViewModels/MainPageViewModel.cs
+++ SelectListDemo/SelectListDemo/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,18 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.Generic;
+using MvvmHelpers;
+using Prism.Mvvm;
+using Prism.Navigation;
 
 
 namespace SelectListDemo.ViewModels
 {
-    public class MainPageViewModel : BindableBase
+    public class MainPageViewModel : BindableBase, INavigatingAware
     {
+        public ObservableRangeCollection<string> Items { get; } = new ObservableRangeCollection<string>();
 
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+            Items.ReplaceRange(new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" });
+        }
     }
 }
