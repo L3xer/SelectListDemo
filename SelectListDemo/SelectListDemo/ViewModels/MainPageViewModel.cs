0a4b3d608a825526d686f868c6c8ea36923b0dc9
Sanya <sanya2371@gmail.com>
2017-11-03 20:47:15 +0200

    Add item tapped command.

--- SelectListDemo/SelectListDemo/ViewModels/MainPageViewModel.cs
+++ SelectListDemo/SelectListDemo/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Diagnostics;
+using System.Collections.Generic;
 using MvvmHelpers;
 using Prism.Mvvm;
+using Prism.Commands;
 using Prism.Navigation;
 
 
@@ -8,7 +10,19 @@ namespace SelectListDemo.ViewModels
 {
     public class MainPageViewModel : BindableBase, INavigatingAware
     {
+        public DelegateCommand ItemTappedCommand { get; set; }
+
         public ObservableRangeCollection<string> Items { get; } = new ObservableRangeCollection<string>();
+
+        public MainPageViewModel()
+        {
+            ItemTappedCommand = new DelegateCommand(ItemTappedExecute);
+        }
+
+        private void ItemTappedExecute()
+        {
+            Debug.WriteLine("ItemTappedExecute");
+        }
 
         public void OnNavigatingTo(NavigationParameters parameters)
         {
